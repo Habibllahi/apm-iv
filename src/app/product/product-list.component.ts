e2b712522480d8fec33c69b7a2fa1391188a1652
unknown <habibllahi3@gmail.com>
2023-04-24 16:00:08 -0500

    updae

--- src/app/product/product-list.component.ts
+++ src/app/product/product-list.component.ts
@@ -1,13 +1,30 @@
-import { Component } from "@angular/core";
+import { Component, OnChanges, OnInit, SimpleChanges } from "@angular/core";
 import { Product } from "src/types/Product";
 import products from "../../api/products/products.json"
 
 @Component({
   templateUrl: "./product-list.component.html",
-  selector: "pm-products"
+  selector: "pm-products",
+  styleUrls:['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
+  ngOnInit(): void {
+    
+  }
+
   public productList: String = "Product List";
   public products: Product[] = products
+  public imageWeight: number = 50;
+  public imageMagine: number  = 2;
+  public showImage: boolean = false;
+  public buttonLable: string = this.showImage? "Hide Image" : "Show Image";
+  public productFilter: string = 'cart';
+
+
+  public toggleImage(){
+      this.showImage = !this.showImage;
+      this.buttonLable = this.showImage? "Hide Image" : "Show Image";
+  }
+
 
 }
